03fdb734b8acfcf39fdf8af14d6ab611655fc6dd
Joao Lucas <joaolucasbezerra02@gmail.com>
2024-02-14 01:05:34 -0300

    code createToken  function

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,17 @@ import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { UserModule } from "src/user/user.module";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthService } from "./auth.service";
 
 @Module({
     imports: [
         JwtModule.register({
-        secret: "7#8k?R_[JWDzKlc34?W7j2@C&5V,#[Iq"
+        secret: `7#8k?R_[JWDzKlc34?W7j2@C&5V,#[Iq`
         }),
         UserModule,
         PrismaModule
     ],
-    controllers: [AuthController]
+    controllers: [AuthController],
+    providers: [AuthService]
 })
 export class AuthModule {}
